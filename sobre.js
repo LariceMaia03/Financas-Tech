f3d2b4fd428a7a17f744e1b53233a9a58b4d3015
Cledison Viana <cledison.viana.ti@gmail.com>
2025-05-20 23:53:15 -0300

    Delete sobre.js

--- sobre.js
+++
@@ -1,7 +0,0 @@
-// sobre.js
-
-// Alternar tema claro/escuro
-const themeToggle = document.querySelector('.theme-toggle');
-themeToggle.addEventListener('click', () => {
-  document.body.classList.toggle('dark');
-});
